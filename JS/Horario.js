46ea9e082e2a094ca1783632adb4027f594ca2e8
David <david.navarrete@est.ulsa.edu.ni>
2026-02-18 20:57:38 -0600

    V5.2_Boton_Funcional_Estructura_Basica_JS_HTML

--- JS/Horario.js
+++ JS/Horario.js
@@ -4,9 +4,29 @@ const contenedor = document.getElementById("contenedorHorario");
 let vistaActual = "lista";
 const tablaOriginal = contenedor.innerHTML;
 
+function renderCalendarioVacio() {
+ contenedor.innerHTML = `
+ <div class="table-responsive">
+  <table class="table table-bordered text-center align-middle">
+   <thead class="table-light">
+    <tr>
+     <th>Hora</th>
+     <th>Lunes</th>
+     <th>Martes</th>
+     <th>Miércoles</th>
+     <th>Jueves</th>
+     <th>Viernes</th>
+    </tr>
+   </thead>
+   <tbody></tbody>
+  </table>
+ </div>
+ `;
+}
+
 btnVista.addEventListener("click", () => {
  if (vistaActual === "lista") {
-  contenedor.innerHTML = "";
+  renderCalendarioVacio();
   btnVista.textContent = "Vista Lista";
   btnVista.classList.remove("btn-primary");
   btnVista.classList.add("btn-outline-primary");
