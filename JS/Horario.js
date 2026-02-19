354d78c11c4aa7c0f7da9bfad71936d43cb758bc
David <david.navarrete@est.ulsa.edu.ni>
2026-02-18 20:56:28 -0600

    V1.1_Boton_Funcional_JS_HTML

--- JS/Horario.js
+++ JS/Horario.js
@@ -1,2 +1,21 @@
 const btnVista = document.getElementById("btnVista");
 const contenedor = document.getElementById("contenedorHorario");
+
+let vistaActual = "lista";
+const tablaOriginal = contenedor.innerHTML;
+
+btnVista.addEventListener("click", () => {
+ if (vistaActual === "lista") {
+  contenedor.innerHTML = "";
+  btnVista.textContent = "Vista Lista";
+  btnVista.classList.remove("btn-primary");
+  btnVista.classList.add("btn-outline-primary");
+  vistaActual = "calendario";
+ } else {
+  contenedor.innerHTML = tablaOriginal;
+  btnVista.textContent = "Vista Calendario";
+  btnVista.classList.remove("btn-outline-primary");
+  btnVista.classList.add("btn-primary");
+  vistaActual = "lista";
+ }
+});
